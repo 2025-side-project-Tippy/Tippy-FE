612dae8e3652f01965f0d6ad4efd10d96db6c15f
soyeong <mb535622@sookmyung.ac.kr>
2025-07-28 02:12:53 +0900

    chore: 라우팅 동작 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 // App.jsx
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MyPage from './MyPage/mypage';
+import Login from './Auth/pages/login';
 
 function App() {
-  return <MyPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MyPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
